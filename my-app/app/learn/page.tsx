0411c80899f48e3c78a630440da27ac93648ed19
Dhani0UM <kleinborous@gmail.com>
2026-04-30 15:11:55 +0800

    lanjut

--- my-app/app/learn/page.tsx
+++ my-app/app/learn/page.tsx
@@ -1,3 +1,43 @@
 export default function learnpage(){
-    return <p>Ini adalah halaman learn yah</p>
+    return (
+
+        <div className="bg-white h-screen flex gap-4 justify-center">
+
+
+        <div className="bg-purple-300 h-72 w-96 flex flex-col justify-between p-8 rounded-4xl text-blue-950">
+            <div>
+                <img src="/palette.svg"/>
+                <p>Design</p>
+          </div>
+          <div>
+            <h1 className="text-4xl font-bold">FIGMA</h1>
+            <p className="text-lg"> 8 hours ago</p>
+          </div>
+        </div>
+
+        <div className="bg-purple-300 h-72 w-96 flex flex-col justify-between p-8 rounded-4xl text-blue-950">
+            <div>
+                <img src="/palette.svg"/>
+                <p>Design</p>
+          </div>
+          <div>
+            <h1 className="text-4xl font-bold">FIGMA</h1>
+            <p className="text-lg"> 8 hours ago</p>
+          </div>
+        </div>
+
+        <div className="bg-purple-300 h-72 w-96 flex flex-col justify-between p-8 rounded-4xl text-blue-950">
+            <div>
+                <img src="/palette.svg"/>
+                <p>Design</p>
+          </div>
+          <div>
+            <h1 className="text-4xl font-bold">FIGMA</h1>
+            <p className="text-lg"> 8 hours ago</p>
+          </div>
+        </div>
+    </div>
+
+    )
 }
+
